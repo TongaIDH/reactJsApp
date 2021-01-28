b114b89033356009a0566d81597b70c0c8333875
Gabriel Alvarez <gastonfga@hotmail.com>
2021-01-28 19:49:28 -0300

    added fakeApi to project

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState'
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const API = 'http://localhost:3000/initialState'
 
-        <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>     
-        </Categories>
+const App = () => {
+    const initialState = useInitialState(API);
+    return initialState.length === 0 ? <h1>Loading...</h1> : (
+        <div className="App">
+            <Header />
+            <Search />
+            {initialState.mylist.length > 0 &&
+                <Categories title="My List">
+                    <Carousel>
+                        {initialState.mylist.map(item =>
+                            <CarouselItem key={item.id} {...item} />
+                        )}
+                    </Carousel>     
+                </Categories>
+            }
+            
+            <Categories title="Trends">
+                <Carousel>
+                    {initialState.trends.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>     
+            </Categories>
 
-        <Categories title="Estrenos">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>     
-        </Categories>
+            <Categories title="Tonga's Video Originals">
+                <Carousel>
+                    {initialState.originals.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>     
+            </Categories>
 
-        <Categories title="Más vistas">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>     
-        </Categories>
-
-        <Footer />
-    </div>
-);
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
